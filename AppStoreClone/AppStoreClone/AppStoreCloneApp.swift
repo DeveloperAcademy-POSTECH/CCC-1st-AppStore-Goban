32f19b21449b1da5f6316ec99a275d028e6e8a9c
Lee Myeonghwan <leemyeonghwan@Gobans-MacBook.local>
2022-05-31 20:18:53 +0900

    create project

---
+++ AppStoreClone/AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by Lee Myeonghwan on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
